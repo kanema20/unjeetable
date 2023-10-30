46aa44df396c4898b02e8368c33fce4e21a90542
kane <zeropointdigit@gmail.com>
2023-10-30 18:39:47 -0400

    venmo option added

--- packages/foundry/contracts/calcium.sol
+++ packages/foundry/contracts/calcium.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract Calcium is ERC20 {
-    constructor() ERC20("Calcium", "CAL") {
-        _mint(msg.sender, 420690000 * 10 ** decimals());
+    constructor() ERC20("Calcium Max", "XCAL") {
+        _mint(msg.sender, 4206900000 * 10 ** decimals());
     }
 }
