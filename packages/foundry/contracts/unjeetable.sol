5f093871244da67ef8c4ad8991ac8b9dd3daedda
kane <zeropointdigit@gmail.com>
2023-11-01 16:38:20 -0400

    set up helper funcs

--- packages/foundry/contracts/unjeetable.sol
+++ packages/foundry/contracts/unjeetable.sol
@@ -2,22 +2,26 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "lib/openzeppelin-contracts/contracts/proxy/utils/Initializable.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "lib/openzeppelin-contracts/contracts/";
 
 // 
 
 contract Unjeetable is Initializable {
     // globals
     // uint256 public x;
-    mapping (address => address) public lockedTokenList;
-    mapping (uint256 => (mapping (address => uint256))) public lockedTokenAmounts;
+    address public admin;
+    address[] public approvedTokens;
+    mapping (address => address) public userLockedTokenList;
+    mapping (uint256 => mapping (address => uint256)) public lockedTokenAmounts;
     // TODO: create 2 mappings, one is address -> token -> amount
     // mapping #2: duration of lock for
 
     // Events
     event Event_LockItUp(address indexed _from, address indexed token_, uint256 amount_, uint256 indexed hoursLocked);
 
-    function initialize() public initializer {
-        // x = _x;
+    function initialize(address admin_) public initializer {
+        admin = admin_;
     }
 
     function lockItUpV1(address token_, uint256 amount_, uint256 hoursLocked) payable public {
@@ -26,11 +30,40 @@ contract Unjeetable is Initializable {
         require(amount_ > 0, "amount_ must be greater than 0");
         require(token_ != address(0), "token_ must not be the zero address");
         // TODO: check if the token is an ERC20 token, allow transfer of tokens to this contract
-        // TODO: create entry in mapping
+        
+        // TODO: create entry in mapping (address => token => amount)
 
         uint256 unlockTimestamp = block.timeStamp + (hoursLocked * 60 * 60);
 
         emit Event_LockItUp(msg.sender, token_, amount_, hoursLocked);
         return ;
     }
+
+    function transferERC20(address token_, ) public {
+
+    }
+
+    function checkIfTokenInList(address token_) internal {
+        // loop through all `store` items until the item with the corresponding ID is found
+        for (uint i; i < .length; i++) {
+            if (store[i].id == id) {
+                // corresponding item found - update quantity and early return
+                store[i].quantity += quantity;
+                return;
+            }
+        }
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can call this function");
+    }
+
+    function addTokenToApprovedList(address token_) internal onlyAdmin() {
+
+    }
+
+        /**
+     * Function that allows the contract to receive ETH
+     */
+    receive() external payable {}
 }
